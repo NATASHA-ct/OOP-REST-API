fec7ed760253d7b17af95204002dff6258d8e247
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-02-27 10:52:14 +0200

    Add specific endpoints for products

--- index.php
+++ index.php
@@ -1,6 +1,39 @@
-<!-- API endpoints -->
-
 <?php
 
-// Path: api.php
+declare(strict_types=1);
+
+// autoloader
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/src/$class.php";
+});
+
+
+// error handling
+set_error_handler("ErrorHandler::handleError");
+set_exception_handler("ErrorHandler::handleException");
+
+// specifying a specific header
+header("Content-type: application/json; charset=UTF-8");
+
+
+// <!-- API endpoints -->
+
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
+
+
+if ($parts[1] != "products") {
+    http_response_code(404);
+    exit;
+}
+
+
+// instantiating classes/ create objects for the classes
+$id = $parts[2] ?? null;
+
+$database = new Database("localhost", "product_db", "root", "");
+
+$gateway = new ProductGateway($database);
+
+$controller = new ProductController($gateway);
+
+$controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
